3e54d55c5755fcc3441542604a881d12400e9abd
techcroft1 <sandeshkhairnar123@gmail.com>
2026-03-18 01:11:05 +0530

    fix the route

--- client/src/app/auth/callback/route.ts
+++ client/src/app/auth/callback/route.ts
@@ -16,5 +16,5 @@ export async function GET(request: NextRequest) {
     }
   }
 
-  return NextResponse.redirect(`${origin}/login?error=auth-callback-failed`);
+  return NextResponse.redirect(`${origin}/sign-in?error=auth-callback-failed`);
 }
